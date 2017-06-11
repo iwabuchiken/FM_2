a6357c3bfef49615e18691a9e7250075ef2e54a6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-12 06:34:05 +0900

    note pc
    new map
    ------------
    20170612_063359
     Changes to be committed:
    	new file:   Music_3/Music_3.mm
    	new file:   q.bat

---
+++ Music_3/Music_3.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1497216593138" ID="ID_1388864154" MODIFIED="1497216693041" TEXT="Music_3">
+<node CREATED="1497216600859" ID="ID_1726019875" MODIFIED="1497216607310" POSITION="right" TEXT="&#x5408;&#x3046;&#x97f3;">
+<node CREATED="1497216749885" ID="ID_1459238181" MODIFIED="1497216749891" TEXT=" 17/06/12"/>
+</node>
+</node>
+</map>
